93452e6fbbabcbc10b1bd477a2d196a369eb1f72
milu281918 <milu281918@gmail.com>
2024-06-02 16:30:17 +0600

    first

--- src/components/Benifits.jsx
+++ src/components/Benifits.jsx
@@ -1,66 +1,61 @@
-import React, { useState } from "react";
+import React from "react";
 import './Card.css'
 function Benifits() {
-const [set,setValue]=useState(false);
+
     return (
       <>
       <div className="benifits">
            <div className="benefits-text">
            <h2 >Benefits of working with us</h2>
            </div>
-        <input type="checkbox" name="isyes" value ="1 " onClick={onclick=>setValue(true)} />
-        <input type="checkbox" name="isyes" value ="0 " onClick={onclick=>setValue(false)} />
-        {visible &&
-        <div className="grid">
-        <div className="card">
-            <div className="row">
-                <div className="img1"><h2 style={{color:'white'}}>01</h2></div>
-                <div className="text-card">
-                    <h3>visualize it</h3>
-                    <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore maxime magni totam esse rem</h4>
+           <div className="grid">
+            <div className="card">
+                <div className="row">
+                    <div className="img1"><h2 style={{color:'white'}}>01</h2></div>
+                    <div className="text-card">
+                        <h3>visualize it</h3>
+                        <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore maxime magni totam esse rem</h4>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="img1"><h2 style={{color:'white'}}>02</h2></div>
+                    <div className="text-card">
+                        <h3>visualize it</h3>
+                        <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore maxime magni totam esse rem</h4>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="img1"><h2 style={{color:'white'}}>03</h2></div>
+                    <div className="text-card">
+                        <h3>visualize it</h3>
+                        <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore maxime magni totam esse rem</h4>
+                    </div>
                 </div>
             </div>
-            <div className="row">
-                <div className="img1"><h2 style={{color:'white'}}>02</h2></div>
-                <div className="text-card">
-                    <h3>visualize it</h3>
-                    <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore maxime magni totam esse rem</h4>
+            <div className="card">
+                <div className="row">
+                    <div className="img1"><h2 style={{color:'white'}}>04</h2></div>
+                    <div className="text-card">
+                        <h3>visualize it</h3>
+                        <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore maxime magni totam esse rem</h4>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="img1"><h2 style={{color:'white'}}>05</h2></div>
+                    <div className="text-card">
+                        <h3>visualize it</h3>
+                        <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore maxime magni totam esse rem</h4>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="img1"><h2 style={{color:'white'}}>06</h2></div>
+                    <div className="text-card">
+                        <h3>visualize it</h3>
+                        <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore maxime magni totam esse rem</h4>
+                    </div>
                 </div>
             </div>
-            <div className="row">
-                <div className="img1"><h2 style={{color:'white'}}>03</h2></div>
-                <div className="text-card">
-                    <h3>visualize it</h3>
-                    <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore maxime magni totam esse rem</h4>
-                </div>
-            </div>
-        </div>
-        <div className="card">
-            <div className="row">
-                <div className="img1"><h2 style={{color:'white'}}>04</h2></div>
-                <div className="text-card">
-                    <h3>visualize it</h3>
-                    <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore maxime magni totam esse rem</h4>
-                </div>
-            </div>
-            <div className="row">
-                <div className="img1"><h2 style={{color:'white'}}>05</h2></div>
-                <div className="text-card">
-                    <h3>visualize it</h3>
-                    <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore maxime magni totam esse rem</h4>
-                </div>
-            </div>
-            <div className="row">
-                <div className="img1"><h2 style={{color:'white'}}>06</h2></div>
-                <div className="text-card">
-                    <h3>visualize it</h3>
-                    <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore maxime magni totam esse rem</h4>
-                </div>
-            </div>
-        </div>
-       </div>
-        }
-           
+           </div>
         </div>
       </>
     );
